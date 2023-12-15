c4cc90b60ca5529d65d39c546c906c80400e8f23
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2023-12-15 18:48:08 -0300

    added pagination, filters and cors

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -12,9 +12,24 @@ namespace Infrastructure.Data
         {
             var query = inputQuery;
 
-            if (spec.Criteria != null)
+            if (spec.Criteria is not null)
             {
                 query = query.Where(spec.Criteria);
+            }
+
+            if (spec.OrderBy is not null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending is not null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
             }
 
             query = spec.Includes
