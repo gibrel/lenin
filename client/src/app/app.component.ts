46b37dc0ac48be2c0c2a63103ca9890575e6487f
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2023-12-19 10:49:19 -0300

    added workin user interface for store

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,8 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'lenin';
-  products: Product[] = [];
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (response: any) => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => {
-        console.log('request complete');
-        console.log('extra statement');
-      }
-    });
-  }
+  ngOnInit(): void { }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,46 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+import { CURRENCY_MASK_CONFIG, CurrencyMaskConfig, CurrencyMaskModule } from 'ng2-currency-mask';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
+  align: 'right',
+  allowNegative: false,
+  decimal: ',',
+  precision: 2,
+  prefix: 'R$ ',
+  suffix: '',
+  thousands: '.'
+}
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule,
+    CurrencyMaskModule
   ],
-  providers: [],
+  providers: [
+    { provide: CURRENCY_MASK_CONFIG, useValue: CustomCurrencyMaskConfig },
+    { provide: LOCALE_ID, useValue: "pt-BR" },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
